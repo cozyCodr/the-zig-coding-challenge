9642aaef02f51af270e900aefa7f43a2989f63f6
Bright Londa <66696421+cozyCodr@users.noreply.github.com>
2022-09-25 21:19:06 +0200

    created popular movies endpoint

--- serverside/Controllers/MoviesController.cs
+++ serverside/Controllers/MoviesController.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using serverside.Models;
 
 namespace serverside.Controllers
 {
@@ -10,11 +13,37 @@ namespace serverside.Controllers
     [ApiController]
     public class MoviesController : ControllerBase
     {
-        // GET api/popular
-        [HttpGet("popular")]
-        public ActionResult<IEnumerable<string>> Get()
+        HttpClientHandler _clientHandler = new HttpClientHandler();
+
+        readonly string api_key = Environment.GetEnvironmentVariable("API_KEY");
+        Movie _oMovie = new Movie();
+        List<Movie> _oPopularMovies = new List<Movie>();
+        List<Root> _oApiResponse = new List<Root>();
+
+        public MoviesController()
         {
-            return new string[] { "popular1", "popular2" };
+            _clientHandler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; };
+        }
+
+
+        
+        // GET api/popular
+        [HttpGet("response")]
+        public async Task<List<Root>> GetPopularMovies()
+        {
+            _oApiResponse = new List<Root>();
+
+            using (var httpClient = new HttpClient(_clientHandler)) 
+            { 
+                using (var response=await httpClient.GetAsync(string.Format("https://api.themoviedb.org/3/trending/movie/day?api_key={0}", api_key)))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(apiResponse);
+                    _oApiResponse.Add(myDeserializedClass);
+                }
+            }
+
+            return _oApiResponse;
         }
 
         // GET api/movie/2
@@ -26,7 +55,7 @@ namespace serverside.Controllers
 
         // GET api/search
         [HttpGet("search")]
-        public void Post([FromQuery(Name ="query")] string query)
+        public void Get([FromQuery(Name ="query")] string query)
         {
         }
 
--- serverside/Models/JsonResponse.cs
+++ serverside/Models/JsonResponse.cs
@@ -5,11 +5,32 @@ using System.Threading.Tasks;
 
 namespace serverside.Models
 {
-    public class JsonResponse
+    public class Result
     {
-        public int Page { get; set; }
-        public List<Movie> Results { get; set; }
-        public int TotalPages { get; set; }
-        public int TotalResults { get; set; }
+        public bool adult { get; set; }
+        public string backdrop_path { get; set; }
+        public int id { get; set; }
+        public string title { get; set; }
+        public string original_language { get; set; }
+        public string original_title { get; set; }
+        public string overview { get; set; }
+        public string poster_path { get; set; }
+        public string media_type { get; set; }
+        public List<int> genre_ids { get; set; }
+        public double popularity { get; set; }
+        public string release_date { get; set; }
+        public bool video { get; set; }
+        public double vote_average { get; set; }
+        public int vote_count { get; set; }
     }
+
+    public class Root
+    {
+        public int page { get; set; }
+        public List<Result> results { get; set; }
+        public int total_pages { get; set; }
+        public int total_results { get; set; }
+    }
+
+
 }
