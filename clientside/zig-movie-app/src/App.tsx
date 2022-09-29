992427b5ade2c6be6ebfaba64f35505a86efa535
Bright Londa <66696421+cozyCodr@users.noreply.github.com>
2022-09-29 15:09:22 +0200

    fixed router issue

--- clientside/zig-movie-app/src/App.tsx
+++ clientside/zig-movie-app/src/App.tsx
@@ -1,15 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
-import Movies from './Movies';
+import Movies from './components/Movies';
+import Details from './components/Details'
+import {Route, Routes} from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { Route, Routes } from 'react-router-dom';
 
 export default function App ()
 {
   return (
     <Routes>
       <Route path='/' element={<Movies />} />
-      <Route path='/details' element={<Details />} />
+      <Route path='/details:id' element={<Details />} />
     </Routes>
   )
 };
