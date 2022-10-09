cca64065111b40f198ebbe89ff995c3e09770325
Bright Londa <66696421+cozyCodr@users.noreply.github.com>
2022-10-09 15:18:34 +0200

    added movies logo

--- clientside/zig-movie-app/src/components/Layout.tsx
+++ clientside/zig-movie-app/src/components/Layout.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import  Search from './Search'
-
+import Logo from '../../src/zig-logo.png'
 
 type Props = {
   children:any,
@@ -9,7 +9,12 @@ type Props = {
 const Layout: React.FC<Props> = ({children}) => {
   return (
     <div className="bg-black h-100">
-        <h1 className="text-center pt-2">ZIG Movie App</h1>
+        <div className="d-flex justify-content-center align-items-center p-3">
+          <div style={{width: "40px", height: "40px", borderRadius: "50%", boxShadow: "0 0 0 1px white, 0 0 0 3px black, 0 0 0 4px white"}} className="d-flex justify-content-center align-items-center mx-2">
+            <img style={{width: "20px", height: "20px"}} src={Logo} />
+          </div>
+          <h1 className="text-center pt-2"  style={{fontSize:"4rem"}} >Movies</h1>
+        </div>
         <Search />
         {children}
     </div>
--- clientside/zig-movie-app/src/components/Search.tsx
+++ clientside/zig-movie-app/src/components/Search.tsx
@@ -7,7 +7,7 @@ const Search = (props: Props) => {
     <div className="d-flex justify-content-center align-content-center py-2 rounded-3">
         <form className="w-100 d-flex justify-content-center">
             <label className="d-flex justify-content-center w-100">
-                <input className="p-1 pl-2 pr-3 w-50" type="text" placeholder='Search title'/>
+                <input id="search-box" className="mx-5 p-1 pl-2 pr-3 w-75 bg-black" type="text" placeholder='Search title'/>
             </label>
         </form>
     </div>
