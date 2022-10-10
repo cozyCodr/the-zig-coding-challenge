92b5fd5c28c2bbe1b65e13b51407a1e711ddbf03
Bright Londa <66696421+cozyCodr@users.noreply.github.com>
2022-10-10 18:28:14 +0200

    added test test structure - not working

--- Controllers.Tests/UnitTest1.cs
+++ Controllers.Tests/UnitTest1.cs
@@ -1,14 +1,29 @@
 using System;
 using Xunit;
+using serverside.Controllers;
+using serverside;
+using System.Net.Http;
 
 namespace Controllers.Tests
 {
-    public class UnitTest1
+    public class MoviesControllerTests
     {
+        /* Tests not working yet  */
         [Fact]
         public void Test1()
         {
-            Assert.True(false);
+            /* Arrange */
+            var _httpClient = new HttpClient();
+            var service = new MovieService(_httpClient);
+            var controller = new MoviesController(service);
+
+            /* Act */
+            var _results = controller.GetPopularMovies();
+            var actual = _results.Result.Count;
+            int expected = 5;
+
+            /* Assert */
+            Assert.Equal(expected, actual);
         }
     }
 }
